c3930760bff2e5655a1bcfcf5411b7154183dcd1
Andrey Varlamov <varlamov.and@gmail.com>
2023-03-01 18:30:12 -0800

    Create opengl window

--- src/main.c
+++ src/main.c
@@ -1,9 +1,40 @@
+#include <stdbool.h>
 #include <stdio.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
 int main()
 {
-    int i;
-    i = 10;
-    printf("Hello %d\n", i);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, false);
+
+    GLFWwindow* window = glfwCreateWindow(
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        "METANOIA",
+        NULL,
+        NULL
+    );
+
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    while(1)
+    {
+
+    }
+
+    glfwTerminate();
     return 0;
 }
